6360d63b3e5b63c0c613346fe9c330263c2bf497
GabryanHD <omfga@192.168.1.140>
2020-12-03 21:22:59 +0100

    Exercici 16. Eliminació de la variable bonificacions

--- refactoring/src/refactoring/Client.java
+++ refactoring/src/refactoring/Client.java
@@ -46,12 +46,10 @@ public class Client {
     }
 
     public String informe() {
-	    int bonificacions = 0;
 	    String resultat = "Informe de lloguers del client " +
 	        getNom() +
 	        " (" + getNif() + ")\n";
-	    for (Lloguer lloguer: lloguers) {
-	    	bonificacions += lloguer.bonificacions();
+	    for (Lloguer lloguer: lloguers) {;
 
 	        // composa els resultats d'aquest lloguer
 	        resultat += "\t" +
@@ -63,7 +61,7 @@ public class Client {
 
 	    // afegeix informació final
 	    resultat += "Import a pagar: " + importTotal() + "€\n" +
-	        "Punts guanyats: " + bonificacions + "\n";
+	        "Punts guanyats: " + bonificacionsTotals() + "\n";
 	    return resultat;
 	}
     
@@ -74,5 +72,14 @@ public class Client {
     		total += lloguer.quantitat() * 30;
     	}
     	return total;
+    }
+    
+    public int bonificacionsTotals() {
+    	int bonificacions = 0;
+    	
+    	for (Lloguer lloguer: lloguers) {
+    		bonificacions += lloguer.bonificacions();
+    	}
+    	return bonificacions;
     }
 }
