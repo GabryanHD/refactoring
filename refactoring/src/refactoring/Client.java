5e00045534a960d5364072d908419a4065e6f11d
GabryanHD <omfga@192.168.1.140>
2020-12-03 17:29:31 +0100

    Correcció, informe va a la classe Client

--- refactoring/src/refactoring/Client.java
+++ refactoring/src/refactoring/Client.java
@@ -46,7 +46,52 @@ public class Client {
     }
 
     public String informe() {
-        // de moment en blanc
-        return null;
-    }
+	    double total = 0;
+	    int bonificacions = 0;
+	    String resultat = "Informe de lloguers del client " +
+	        getNom() +
+	        " (" + getNif() + ")\n";
+	    for (Lloguer lloguer: lloguers) {
+	        double quantitat = 0;
+	        switch (lloguer.getVehicle().getCategoria()) {
+	            case 0:
+	                quantitat += 3;
+	                if (lloguer.getDies() > 3) {
+	                    quantitat += (lloguer.getDies() - 3) * 1.5;
+	                }
+	                break;
+	            case 1:
+	                quantitat += 4;
+	                if (lloguer.getDies() > 2) {
+	                    quantitat += (lloguer.getDies() - 2) * 2.5;
+	                }
+	                break;
+	            case 2:
+	                quantitat += lloguer.getDies() * 6;
+	                break;
+	        }
+
+	        // afegeix lloguers freqüents
+	        bonificacions ++;
+
+	        // afegeix bonificació per dos dies de lloguer de Luxe
+	        if (lloguer.getVehicle().getCategoria() == 2 &&
+	                lloguer.getDies()>1 ) {
+	            bonificacions ++;
+	        }
+
+	        // composa els resultats d'aquest lloguer
+	        resultat += "\t" +
+	            lloguer.getVehicle().getMarca() +
+	            " " +
+	            lloguer.getVehicle().getModel() + ": " +
+	            (quantitat * 30) + "€" + "\n";
+	        total += quantitat * 30;
+	    }
+
+	    // afegeix informació final
+	    resultat += "Import a pagar: " + total + "€\n" +
+	        "Punts guanyats: " + bonificacions + "\n";
+	    return resultat;
+	}
 }
